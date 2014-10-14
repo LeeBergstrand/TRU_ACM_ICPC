a23e9c75be4c4b1a26077b08906ba5d16a29b7a9
Lee Bergstrand <leemacboy@gmail.com>
2014-10-14 16:51:43 -0700

    Fixed problems with our java.

--- Questions:Solutions/Question_Two/Question2_Top_Experts.java
+++ Questions:Solutions/Question_Two/Question2_Top_Experts.java
@@ -1,24 +1,31 @@
 import java.util.Scanner;
 
 // This a comment
-public class Question2 {
+public class Question2_Top_Experts {
 
+	static String[] UNITS = {"Wizard", "Warrior", "Ranger", "Elementalist", "Assassin", "Ninja", "Dragon"};
 	static int[] GOOD_WORTH = {1, 2, 3, 3, 4, 10};
 	static int[] EVIL_WORTH = {1, 2, 2, 2, 3, 5, 11};
 	
-	public static void main() {
+	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int good, evil;
-		int battles = scan.nextInt();
+		int battles; 
+		
+		System.out.print("Enter the number of battles: ");
+		
+		battles = scan.nextInt();
 		
 		for (int bi = 0; bi < battles; bi++) {
 			good = evil = 0;
 
 			for (int gi = 0; gi < 6; gi++) {
+				System.out.print("Enter the number of Good " + UNITS[gi] + "s in battle " + bi + ": ");
 				good = scan.nextInt() * GOOD_WORTH[gi];
 			}
 			
 			for (int ei = 0; ei < 7; ei++) {
+				System.out.print("Enter the number of Evil " + UNITS[ei] + "s in battle " + bi + ": ");
 				evil = scan.nextInt() * EVIL_WORTH[ei];
 			}
 			
@@ -30,6 +37,7 @@ public class Question2 {
 			else
 				System.out.println("No victor on this battle field");
 		}
-	}
-	
+		
+		scan.close();
+	}	
 }
